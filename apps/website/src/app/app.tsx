95a9d43a66ea02d82592b2e09b4b50c1a4b87f9c
Matt Gibney <findmoppler@gmail.com>
2024-02-15 21:28:28 +0000

    Add tailwind Remove boilerplate

--- apps/website/src/app/app.tsx
+++ apps/website/src/app/app.tsx
@@ -1,14 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
-
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="website" />
 
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
