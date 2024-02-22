a380b663409b428680f692621c5211e42812e771
Matt Gibney <findmoppler@gmail.com>
2024-02-22 22:45:24 +0000

    Update Footer text

--- apps/web-app/src/components/layout/footer.tsx
+++ apps/web-app/src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="mx-auto max-w-7xl px-6 py-12 flex items-center justify-center lg:px-8">
       <div className="mt-8 md:mt-0">
         <p className="text-center text-xs leading-5 text-zinc-500">
-          &copy; 2024 Digital Yarn Games. All rights reserved.
+          &copy; 2024 DarkThrone Reborn
         </p>
       </div>
     </footer>
