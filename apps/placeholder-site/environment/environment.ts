3c32c664c7a918d4c37e26f85b1d513092fd3204
Matt Gibney <findmoppler@gmail.com>
2024-02-22 22:28:29 +0000

    Populated the placeholder website

---
+++ apps/placeholder-site/environment/environment.ts
@@ -0,0 +1,3 @@
+export default {
+  gameAppUrl: 'http://localhost:4200',
+}
--- apps/placeholder-site/src/app/app.tsx
+++ apps/placeholder-site/src/app/app.tsx
@@ -1,7 +1,25 @@
+import { Button, Logo } from '@darkthrone/react-components';
+import environment from '../../environment/environment';
+
 export function App() {
+  function handlePlayClick() {
+    window.location.href = environment.gameAppUrl;
+  }
+
   return (
-    <div>
-      DarkThrone
+    <div className='max-w-md mx-auto flex flex-col'>
+      <div className='mx-auto mt-12 mb-6 pb-6 border-b border-zinc-600'>
+        <Logo variant='large' />
+      </div>
+
+      <div className='text-zinc-300 mb-6'>
+        <p className='mb-3'>Welcome to DarkThrone Reborn!</p>
+        <p className='mb-3'>This is a passion project that I'm working on in my free time and is currently very much in early-alpha. I wanted to get a Minimum Viable Product put together and then develop it with feedback from the community.</p>
+        <p className='mb-3'>In this limited version of the game, you can have multiple player accounts, train units and attack one another. Over time, functionality will improve and I really look forward to gathering feedback and comments from the players.</p>
+        <p className='mb-3'>I hope you enjoy the game and I'm looking forward to hearing from you!</p>
+      </div>
+
+      <Button text={'Play DarkThrone Reborn'} onClick={handlePlayClick} />
     </div>
   );
 }
