67652992d229b64d27123079595ea19dc6300a5a
Matt Gibney <findmoppler@gmail.com>
2024-02-24 13:21:47 +0000

    Update layout of placeholder site

--- apps/placeholder-site/src/app/app.tsx
+++ apps/placeholder-site/src/app/app.tsx
@@ -7,19 +7,21 @@ export function App() {
   }
 
   return (
-    <div className='max-w-md mx-auto flex flex-col'>
-      <div className='mx-auto mt-12 mb-6 pb-6 border-b border-zinc-600'>
-        <Logo variant='large' />
-      </div>
+    <div className='sm:h-screen sm:grid sm:place-items-center'>
+      <div className='max-w-md mx-auto flex flex-col sm:bg-zinc-800 p-4 rounded-md'>
+        <div className='mx-auto my-6 pb-6 border-b border-zinc-600'>
+          <Logo variant='large' />
+        </div>
 
-      <div className='text-zinc-300 mb-6'>
-        <p className='mb-3'>Welcome to DarkThrone Reborn!</p>
-        <p className='mb-3'>This is a passion project that I'm working on in my free time and is currently very much in early-alpha. I wanted to get a Minimum Viable Product put together and then develop it with feedback from the community.</p>
-        <p className='mb-3'>In this limited version of the game, you can have multiple player accounts, train units and attack one another. Over time, functionality will improve and I really look forward to gathering feedback and comments from the players.</p>
-        <p className='mb-3'>I hope you enjoy the game and I'm looking forward to hearing from you!</p>
-      </div>
+        <div className='text-zinc-300 mb-6'>
+          <p className='mb-3'>Welcome to DarkThrone Reborn!</p>
+          <p className='mb-3'>This is a passion project that I'm working on in my free time and is currently very much in early-alpha. I wanted to get a Minimum Viable Product put together and then develop it with feedback from the community.</p>
+          <p className='mb-3'>In this limited version of the game, you can have multiple player accounts, train units and attack one another. Over time, functionality will improve and I really look forward to gathering feedback and comments from the players.</p>
+          <p className='mb-3'>I hope you enjoy the game and I'm looking forward to hearing from you!</p>
+        </div>
 
-      <Button text={'Play DarkThrone Reborn'} onClick={handlePlayClick} />
+        <Button text={'Play DarkThrone Reborn'} onClick={handlePlayClick} />
+      </div>
     </div>
   );
 }
