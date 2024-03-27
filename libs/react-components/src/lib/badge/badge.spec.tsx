35103fb8d0c364a9f56dfbb1632d1af4427f4f7b
Filip Popa <filip.popa@gmail.com>
2024-03-27 23:25:11 +0200

    Fix failing test

--- libs/react-components/src/lib/badge/badge.spec.tsx
+++ libs/react-components/src/lib/badge/badge.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import Button from './button';
+import Button from '../button/button';
 
 describe('Button', () => {
   it('should render successfully', () => {
