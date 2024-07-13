0eab2f137a50a4ab169c9557216deb7e80f93546
Matt Gibney <findmoppler@gmail.com>
2024-07-13 14:41:19 +0100

    Update HeaderBar to fix broken Player Switching

--- apps/web-app/src/components/layout/headerBar.tsx
+++ apps/web-app/src/components/layout/headerBar.tsx
@@ -1,8 +1,8 @@
 import DarkThroneClient from '@darkthrone/client-library';
 import { Avatar } from '@darkthrone/react-components';
-import { Menu, Transition } from '@headlessui/react';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { Bars3Icon, ChevronDownIcon } from '@heroicons/react/20/solid';
-import { Fragment, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 interface HeaderBarProps {
@@ -93,75 +93,53 @@ export default function HeaderBar(props: HeaderBarProps) {
           ) : null}
         </div>
         <div className="ml-auto flex items-center gap-x-4 lg:gap-x-6">
-          {/* <button type="button" className="-m-2.5 p-2.5 text-zinc-300 hover:text-zinc-400">
-            <span className="sr-only">View notifications</span>
-            <BellIcon className="h-6 w-6" aria-hidden="true" />
-          </button> */}
-
-          {/* Separator */}
-          {/* <div className="hidden lg:block lg:h-6 lg:w-px lg:bg-zinc-700" aria-hidden="true" /> */}
-
           {/* Profile dropdown */}
-          <Menu as="div" className="relative">
-            <Menu.Button className="-m-1.5 flex items-center p-1.5">
-              <span className="sr-only">Open user menu</span>
+          <Menu as="div" className="relative ml-3">
+            <div>
+              <MenuButton className="relative flex items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-zinc-600 focus:ring-offset-2 focus:ring-offset-gray-800">
+                <span className="absolute -inset-1.5" />
+                <span className="sr-only">Open user menu</span>
 
-              <Avatar
-                race={props.client.authenticatedPlayer?.race}
-                url={props.client.authenticatedPlayer?.avatarURL}
-                size="small"
-              />
-
-              <span className="hidden lg:flex lg:items-center">
-                <span
-                  className="ml-4 text-sm font-semibold leading-6 text-zinc-300"
-                  aria-hidden="true"
-                >
-                  {props.client.authenticatedPlayer?.name}
-                </span>
-                <ChevronDownIcon
-                  className="ml-2 h-5 w-5 text-zinc-300"
-                  aria-hidden="true"
+                <Avatar
+                  race={props.client.authenticatedPlayer?.race}
+                  url={props.client.authenticatedPlayer?.avatarURL}
+                  size="small"
                 />
-              </span>
-            </Menu.Button>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-zinc-800 py-2 shadow-lg ring-1 ring-zinc-600/95 focus:outline-none">
-                <Menu.Button
-                  className="w-full items-center text-left rounded-md px-2 py-2 text-sm"
+
+                <span className="hidden lg:flex lg:items-center">
+                  <span
+                    className="ml-4 text-sm font-semibold leading-6 text-zinc-300"
+                    aria-hidden="true"
+                  >
+                    {props.client.authenticatedPlayer?.name}
+                  </span>
+                  <ChevronDownIcon
+                    className="ml-2 h-5 w-5 text-zinc-300"
+                    aria-hidden="true"
+                  />
+                </span>
+              </MenuButton>
+            </div>
+            <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-zinc-700 py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
+              <MenuItem>
+                <button
+                  type="button"
+                  className="block w-full text-left px-4 py-2 text-sm text-gray-300 data-[focus]:bg-zinc-800/50"
                   onClick={() => handleSwitchPlayer()}
                 >
                   Switch Players
-                </Menu.Button>
-                <Menu.Button
-                  className="w-full items-center text-left rounded-md px-2 py-2 text-sm"
+                </button>
+              </MenuItem>
+              <MenuItem>
+                <button
+                  type="button"
+                  className="block w-full text-left px-4 py-2 text-sm text-gray-300 data-[focus]:bg-zinc-800/50"
                   onClick={() => props.client.auth.logout()}
                 >
                   Logout
-                </Menu.Button>
-                {/* <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href={item.href}
-                        className={classNames(
-                          active ? 'bg-zinc-50' : '',
-                          'block px-3 py-1 text-sm leading-6 text-zinc-900'
-                        )}
-                      >
-                        {item.name}
-                      </a>
-                    )}
-                  </Menu.Item> */}
-              </Menu.Items>
-            </Transition>
+                </button>
+              </MenuItem>
+            </MenuItems>
           </Menu>
         </div>
       </div>
