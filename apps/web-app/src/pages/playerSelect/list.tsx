64661f54e28748970f992e850047d7ab1311f9ad
Matt Gibney <findmoppler@gmail.com>
2025-12-17 20:48:47 +0000

    feat: refactor PlayerSelectListPage to use Shadcn components and improve UI styling

--- apps/web-app/src/pages/playerSelect/list.tsx
+++ apps/web-app/src/pages/playerSelect/list.tsx
@@ -1,7 +1,8 @@
 import DarkThroneClient from '@darkthrone/client-library';
 import { PlayerObject } from '@darkthrone/interfaces';
-import { Avatar, Button, Logo } from '@darkthrone/react-components';
-import { ChevronRightIcon } from '@heroicons/react/20/solid';
+import { Avatar, Logo } from '@darkthrone/react-components';
+import { Button } from '@darkthrone/shadcnui/button';
+import { ChevronRightIcon } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -44,37 +45,34 @@ export default function PlayerSelectListPage(props: PlayerSelectListPageProps) {
         <div className="flex justify-center">
           <Logo variant="large" />
         </div>
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-zinc-400">
-          Player Select
-        </h2>
       </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-        <div className="bg-zinc-800 sm:rounded-t-lg px-6 py-12 sm:px-12">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-120">
+        <div className="bg-muted sm:rounded-t-lg px-6 py-12 sm:px-12">
           {players.length > 0 ? (
             <>
               <div>
                 {players.map((player) => (
                   <button
                     key={player.id}
-                    className="w-full text-left relative flex justify-between items-center gap-x-6 px-4 py-5 hover:bg-zinc-700/50 rounded-lg sm:px-6"
+                    className="w-full text-left relative flex justify-between items-center gap-x-6 px-4 py-5 hover:bg-background/25 active:bg-background/35 rounded-lg sm:px-6"
                     onClick={() => handleChoosePlayer(player)}
                   >
                     <div className="flex items-center min-w-0 gap-x-4">
                       <Avatar url={player.avatarURL} race={player.race} />
 
                       <div className="min-w-0 flex-auto">
-                        <p className="text-sm font-semibold leading-6 text-zinc-200 capitalize">
+                        <p className="text-sm font-semibold leading-6 text-foreground">
                           {player.name}
                         </p>
-                        <p className="flex text-sm leading-5 text-zinc-500 capitalize">
+                        <p className="flex text-sm leading-5 text-foreground/50 capitalize">
                           {player.race} {player.class}
                         </p>
                       </div>
                     </div>
                     <div className="flex shrink-0 items-center gap-x-4">
                       <ChevronRightIcon
-                        className="h-5 w-5 flex-none text-zinc-400"
+                        className="h-5 w-5 flex-none text-foreground/50"
                         aria-hidden="true"
                       />
                     </div>
@@ -87,10 +85,10 @@ export default function PlayerSelectListPage(props: PlayerSelectListPageProps) {
                   className="absolute inset-0 flex items-center"
                   aria-hidden="true"
                 >
-                  <div className="w-full border-t border-zinc-700" />
+                  <div className="w-full border-t border-foreground/15" />
                 </div>
                 <div className="relative flex justify-center">
-                  <span className="bg-zinc-800 px-2 text-sm text-zinc-500">
+                  <span className="bg-muted px-2 text-sm text-foreground/40">
                     Or
                   </span>
                 </div>
@@ -100,29 +98,30 @@ export default function PlayerSelectListPage(props: PlayerSelectListPageProps) {
 
           <Button
             type="button"
-            text={'Create a new player'}
             onClick={() => navigate('/player-select/create')}
-            variant={players.length > 0 ? 'secondary' : 'primary'}
-          />
-          {/* <button className=' w-full p-6 rounded-lg'>
+            variant={players.length > 0 ? 'outline' : 'default'}
+            className="w-full py-7 border-0"
+            size={'lg'}
+          >
             Create a new player
-          </button> */}
+          </Button>
         </div>
-        <div className="bg-black/25 sm:rounded-b-lg p-6 sm:px-12 flex justify-between items-center">
+        <div className="bg-muted/50 sm:rounded-b-lg p-6 sm:px-12 flex justify-between items-center">
           <div>
-            <p className="text-sm text-zinc-400">Signed in as</p>
-            <p className="truncate text-sm font-bold text-zinc-200">
+            <p className="text-sm text-foreground/70">Signed in as</p>
+            <p className="truncate text-sm font-bold text-foreground">
               {props.client.authenticatedUser?.email}
             </p>
           </div>
 
           <div>
             <Button
-              text={'Logout'}
               variant="secondary"
               type="button"
               onClick={() => props.client.auth.logout()}
-            />
+            >
+              Logout
+            </Button>
           </div>
         </div>
       </div>
