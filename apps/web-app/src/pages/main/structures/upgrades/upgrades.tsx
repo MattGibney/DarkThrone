b5e259e320e80f95a6329dcda85fa5f0f5cfb31e
Matt Gibney <findmoppler@gmail.com>
2025-12-20 15:51:20 +0000

    UI Refresh for the Structure Upgrades Page

--- apps/web-app/src/pages/main/structures/upgrades/upgrades.tsx
+++ apps/web-app/src/pages/main/structures/upgrades/upgrades.tsx
@@ -4,9 +4,10 @@ import {
   housingUpgrades,
   armouryUpgrades,
 } from '@darkthrone/game-data';
-import { Button } from '@darkthrone/react-components';
+import { Button } from '@darkthrone/shadcnui/button';
 import { StructureUpgradeType } from '@darkthrone/interfaces';
 import { useEffect, useState } from 'react';
+import { Card, CardContent } from '@darkthrone/shadcnui/card';
 
 interface UpgradesScreenProps {
   client: DarkThroneClient;
@@ -51,244 +52,246 @@ export default function UpgradesScreen(props: UpgradesScreenProps) {
   }
 
   return (
-    <main className="max-w-7xl mx-auto">
-      <div className="max-w-3xl mx-auto flex flex-col gap-y-8">
-        <div className="bg-zinc-800/50 rounded-lg p-4 flex justify-center gap-x-12 text-zinc-400 text-sm">
+    <main className="grid gap-6 mx-auto max-w-4xl">
+      <Card>
+        <CardContent className="flex justify-center gap-x-12 text-card-foreground/70 text-sm">
           <div>
             Gold{' '}
-            <span className="text-white font-bold text-md">
+            <span className="text-card-foreground font-bold text-md">
               {new Intl.NumberFormat().format(
-                props.client.authenticatedPlayer.gold,
+                props.client.authenticatedPlayer?.gold || 0,
               )}
             </span>
           </div>
           <div>
-            Current Player Level{' '}
-            <span className="text-white font-bold text-md">
+            Citizens{' '}
+            <span className="text-card-foreground font-bold text-md">
               {new Intl.NumberFormat().format(
-                props.client.authenticatedPlayer.level,
+                props.client.authenticatedPlayer?.units.find(
+                  (unit) => unit.unitType === 'citizen',
+                )?.quantity || 0,
               )}
             </span>
           </div>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 rounded-lg overflow-hidden text-center text-zinc-200">
-          <div className="bg-zinc-800 p-8 flex flex-col gap-y-4 text-sm">
-            <h3 className="font-semibold text-lg">Current Fortification</h3>
-            <div>
-              <p className="text-white font-bold">
-                {upgrades.fortification.current.name}
-              </p>
-              <p>
-                Gold Per Turn:{' '}
-                {new Intl.NumberFormat().format(
-                  upgrades.fortification.current.goldPerTurn,
-                )}
-              </p>
-              <p>
-                Defence Bonus:{' '}
-                {upgrades.fortification.current.defenceBonusPercentage}%
-              </p>
-            </div>
-          </div>
-          <div className="bg-zinc-800/50 p-8 flex flex-col gap-y-4 text-sm">
-            <h3 className="font-semibold text-lg">Next Upgrade</h3>
-            {upgrades.fortification.next ? (
-              <>
-                <div>
-                  <p className="text-white font-bold">
-                    {upgrades.fortification.next.name}
-                  </p>
-                  <p>
-                    Gold Per Turn:{' '}
-                    {new Intl.NumberFormat().format(
-                      upgrades.fortification.next.goldPerTurn,
-                    )}
-                  </p>
-                  <p>
-                    Defence Bonus:{' '}
-                    {upgrades.fortification.next.defenceBonusPercentage}%
-                  </p>
-                </div>
+        </CardContent>
+      </Card>
 
-                {props.client.authenticatedPlayer.level <
-                upgrades.fortification.next.levelRequirement ? (
-                  <>
-                    <p>
-                      Cost:{' '}
-                      {new Intl.NumberFormat().format(
-                        upgrades.fortification.next.cost,
-                      )}{' '}
-                      <span className="text-gold">Gold</span>
-                    </p>
-                    <p className="bg-cyan-800/40 border border-cyan-900/80 text-sm font-medium text-cyan-40 p-2 rounded-md">
-                      You need to be level{' '}
-                      {upgrades.fortification.next.levelRequirement} to upgrade
-                    </p>
-                  </>
-                ) : (
-                  <Button
-                    variant="primary-outline"
-                    onClick={() => handleUpgrade('fortification')}
-                    type="button"
-                    isDisabled={
-                      props.client.authenticatedPlayer.gold <
-                      upgrades.fortification.next.cost
-                    }
-                  >
-                    Upgrade for{' '}
+      <div className="bg-muted border border-card-border grid grid-cols-1 sm:grid-cols-2 rounded-lg overflow-hidden text-center text-foreground">
+        <div className="p-8 flex flex-col gap-y-4 text-sm">
+          <h3 className="font-semibold text-lg">Current Fortification</h3>
+          <div>
+            <p className="text-foreground font-bold">
+              {upgrades.fortification.current.name}
+            </p>
+            <p>
+              Gold Per Turn:{' '}
+              {new Intl.NumberFormat().format(
+                upgrades.fortification.current.goldPerTurn,
+              )}
+            </p>
+            <p>
+              Defence Bonus:{' '}
+              {upgrades.fortification.current.defenceBonusPercentage}%
+            </p>
+          </div>
+        </div>
+        <div className="bg-card p-8 flex flex-col gap-y-4 text-sm">
+          <h3 className="font-semibold text-lg">Next Upgrade</h3>
+          {upgrades.fortification.next ? (
+            <>
+              <div>
+                <p className="text-foreground font-bold">
+                  {upgrades.fortification.next.name}
+                </p>
+                <p>
+                  Gold Per Turn:{' '}
+                  {new Intl.NumberFormat().format(
+                    upgrades.fortification.next.goldPerTurn,
+                  )}
+                </p>
+                <p>
+                  Defence Bonus:{' '}
+                  {upgrades.fortification.next.defenceBonusPercentage}%
+                </p>
+              </div>
+
+              {props.client.authenticatedPlayer.level <
+              upgrades.fortification.next.levelRequirement ? (
+                <>
+                  <p>
+                    Cost:{' '}
                     {new Intl.NumberFormat().format(
                       upgrades.fortification.next.cost,
                     )}{' '}
                     Gold
-                  </Button>
-                )}
-              </>
-            ) : (
-              <p className="text-zinc-200">
-                There are currently no more available fortification upgrades
-              </p>
-            )}
+                  </p>
+                  <p className="bg-accent/40 border border-accent/80 text-sm font-medium text-accent-foreground p-2 rounded-md">
+                    You need to be level{' '}
+                    {upgrades.fortification.next.levelRequirement} to upgrade
+                  </p>
+                </>
+              ) : (
+                <Button
+                  variant="default"
+                  size={'lg'}
+                  onClick={() => handleUpgrade('fortification')}
+                  type="button"
+                  disabled={
+                    props.client.authenticatedPlayer.gold <
+                    upgrades.fortification.next.cost
+                  }
+                >
+                  Upgrade for{' '}
+                  {new Intl.NumberFormat().format(
+                    upgrades.fortification.next.cost,
+                  )}{' '}
+                  Gold
+                </Button>
+              )}
+            </>
+          ) : (
+            <p className="text-foreground">
+              There are currently no more available fortification upgrades
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="bg-muted border border-card-border grid grid-cols-1 sm:grid-cols-2 rounded-lg overflow-hidden text-center text-foreground">
+        <div className="p-8 flex flex-col gap-y-4 text-sm">
+          <h3 className="font-semibold text-lg">Current Housing</h3>
+          <div>
+            <p className="text-foreground font-bold">
+              {upgrades.housing.current.name}
+            </p>
+            <p>
+              Daily Citizens:{' '}
+              {new Intl.NumberFormat().format(
+                upgrades.housing.current.citizensPerDay,
+              )}
+            </p>
           </div>
         </div>
+        <div className="bg-card p-8 flex flex-col gap-y-4 text-sm">
+          <h3 className="font-semibold text-lg">Next Housing</h3>
+          {upgrades.housing.next ? (
+            <>
+              <div>
+                <p className="text-foreground font-bold">
+                  {upgrades.housing.next.name}
+                </p>
+                <p>
+                  Daily Citizens:{' '}
+                  {new Intl.NumberFormat().format(
+                    upgrades.housing.next.citizensPerDay,
+                  )}
+                </p>
+              </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 rounded-lg overflow-hidden text-center text-zinc-200">
-          <div className="bg-zinc-800 p-8 flex flex-col gap-y-4 text-sm">
-            <h3 className="font-semibold text-lg">Current Housing</h3>
-            <div>
-              <p className="text-white font-bold">
-                {upgrades.housing.current.name}
-              </p>
-              <p>
-                Daily Citizens:{' '}
-                {new Intl.NumberFormat().format(
-                  upgrades.housing.current.citizensPerDay,
-                )}
-              </p>
-            </div>
-          </div>
-          <div className="bg-zinc-800/50 p-8 flex flex-col gap-y-4 text-sm">
-            <h3 className="font-semibold text-lg">Next Housing</h3>
-            {upgrades.housing.next ? (
-              <>
-                <div>
-                  <p className="text-white font-bold">
-                    {upgrades.housing.next.name}
-                  </p>
+              {props.client.authenticatedPlayer.structureUpgrades
+                .fortification <
+              upgrades.housing.next.requiredFortificationLevel ? (
+                <>
                   <p>
-                    Daily Citizens:{' '}
-                    {new Intl.NumberFormat().format(
-                      upgrades.housing.next.citizensPerDay,
-                    )}
-                  </p>
-                </div>
-
-                {props.client.authenticatedPlayer.structureUpgrades
-                  .fortification <
-                upgrades.housing.next.requiredFortificationLevel ? (
-                  <>
-                    <p>
-                      Cost:{' '}
-                      {new Intl.NumberFormat().format(
-                        upgrades.housing.next.cost,
-                      )}{' '}
-                      <span className="text-gold">Gold</span>
-                    </p>
-                    <p className="bg-cyan-800/40 border border-cyan-900/80 text-sm font-medium text-cyan-40 p-2 rounded-md">
-                      Your fortification must be at least{' '}
-                      {
-                        fortificationUpgrades[
-                          upgrades.housing.next.requiredFortificationLevel
-                        ].name
-                      }{' '}
-                      to upgrade
-                    </p>
-                  </>
-                ) : (
-                  <Button
-                    variant="primary-outline"
-                    onClick={() => handleUpgrade('housing')}
-                    type="button"
-                    isDisabled={
-                      props.client.authenticatedPlayer.gold <
-                      upgrades.housing.next.cost
-                    }
-                  >
-                    Upgrade for{' '}
+                    Cost:{' '}
                     {new Intl.NumberFormat().format(upgrades.housing.next.cost)}{' '}
-                    Gold
-                  </Button>
-                )}
-              </>
-            ) : (
-              <p className="text-zinc-200">
-                There are currently no more available fortification upgrades
-              </p>
-            )}
+                    <span className="text-gold">Gold</span>
+                  </p>
+                  <p className="bg-accent/40 border border-accent/80 text-sm font-medium text-accent-foreground p-2 rounded-md">
+                    Your fortification must be at least{' '}
+                    {
+                      fortificationUpgrades[
+                        upgrades.housing.next.requiredFortificationLevel
+                      ].name
+                    }{' '}
+                    to upgrade
+                  </p>
+                </>
+              ) : (
+                <Button
+                  variant="default"
+                  size={'lg'}
+                  onClick={() => handleUpgrade('housing')}
+                  type="button"
+                  disabled={
+                    props.client.authenticatedPlayer.gold <
+                    upgrades.housing.next.cost
+                  }
+                >
+                  Upgrade for{' '}
+                  {new Intl.NumberFormat().format(upgrades.housing.next.cost)}{' '}
+                  Gold
+                </Button>
+              )}
+            </>
+          ) : (
+            <p className="text-foreground">
+              There are currently no more available fortification upgrades
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="bg-muted border border-card-border grid grid-cols-1 sm:grid-cols-2 rounded-lg overflow-hidden text-center text-foreground">
+        <div className="p-8 flex flex-col gap-y-4 text-sm">
+          <h3 className="font-semibold text-lg">Current Armoury</h3>
+          <div>
+            <p className="text-foreground font-bold">
+              {upgrades.armoury.current.name}
+            </p>
           </div>
         </div>
+        <div className="bg-card p-8 flex flex-col gap-y-4 text-sm">
+          <h3 className="font-semibold text-lg">Next Armoury</h3>
+          {upgrades.armoury.next ? (
+            <>
+              <div>
+                <p className="text-foreground font-bold">
+                  {upgrades.armoury.next.name}
+                </p>
+              </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 rounded-lg overflow-hidden text-center text-zinc-200">
-          <div className="bg-zinc-800 p-8 flex flex-col gap-y-4 text-sm">
-            <h3 className="font-semibold text-lg">Current Armoury</h3>
-            <div>
-              <p className="text-white font-bold">
-                {upgrades.armoury.current.name}
-              </p>
-            </div>
-          </div>
-          <div className="bg-zinc-800/50 p-8 flex flex-col gap-y-4 text-sm">
-            <h3 className="font-semibold text-lg">Next Armoury</h3>
-            {upgrades.armoury.next ? (
-              <>
-                <div>
-                  <p className="text-white font-bold">
-                    {upgrades.armoury.next.name}
-                  </p>
-                </div>
-
-                {props.client.authenticatedPlayer.structureUpgrades
-                  .fortification <
-                upgrades.armoury.next.requiredFortificationLevel ? (
-                  <>
-                    <p>
-                      Cost:{' '}
-                      {new Intl.NumberFormat().format(
-                        upgrades.armoury.next.cost,
-                      )}{' '}
-                      <span className="text-gold">Gold</span>
-                    </p>
-                    <p className="bg-cyan-800/40 border border-cyan-900/80 text-sm font-medium text-cyan-40 p-2 rounded-md">
-                      Your fortification must be at least{' '}
-                      {
-                        fortificationUpgrades[
-                          upgrades.armoury.next.requiredFortificationLevel
-                        ].name
-                      }{' '}
-                      to upgrade
-                    </p>
-                  </>
-                ) : (
-                  <Button
-                    variant="primary-outline"
-                    onClick={() => handleUpgrade('armoury')}
-                    type="button"
-                    isDisabled={
-                      props.client.authenticatedPlayer.gold <
-                      upgrades.armoury.next.cost
-                    }
-                  >
-                    Upgrade for{' '}
+              {props.client.authenticatedPlayer.structureUpgrades
+                .fortification <
+              upgrades.armoury.next.requiredFortificationLevel ? (
+                <>
+                  <p>
+                    Cost:{' '}
                     {new Intl.NumberFormat().format(upgrades.armoury.next.cost)}{' '}
-                    Gold
-                  </Button>
-                )}
-              </>
-            ) : (
-              <p className="text-zinc-200">
-                There are currently no more available armoury upgrades
-              </p>
-            )}
-          </div>
+                    <span className="text-gold">Gold</span>
+                  </p>
+                  <p className="bg-accent/40 border border-accent/80 text-sm font-medium text-accent-foreground p-2 rounded-md">
+                    Your fortification must be at least{' '}
+                    {
+                      fortificationUpgrades[
+                        upgrades.armoury.next.requiredFortificationLevel
+                      ].name
+                    }{' '}
+                    to upgrade
+                  </p>
+                </>
+              ) : (
+                <Button
+                  variant="default"
+                  size={'lg'}
+                  onClick={() => handleUpgrade('armoury')}
+                  type="button"
+                  disabled={
+                    props.client.authenticatedPlayer.gold <
+                    upgrades.armoury.next.cost
+                  }
+                >
+                  Upgrade for{' '}
+                  {new Intl.NumberFormat().format(upgrades.armoury.next.cost)}{' '}
+                  Gold
+                </Button>
+              )}
+            </>
+          ) : (
+            <p className="text-foreground">
+              There are currently no more available armoury upgrades
+            </p>
+          )}
         </div>
       </div>
     </main>
